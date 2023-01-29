f9bbedf526d5d09361b5bb857d9b6a06e52ba4ab
Novian MW <114653877+novianmw@users.noreply.github.com>
2023-01-29 21:26:34 +0700

    edit JS

--- bubbling.js
+++ bubbling.js
@@ -3,24 +3,8 @@ const close = document.querySelectorAll('.close')
 close.forEach(function(el) {
     el.addEventListener('click', function(e){
         e.target.parentElement.style.display = 'none'
-        e.preventDefault() // mencegah aksi defaultnya tidak aktif
-        e.stopPropagation() // menghentikan efek bubbling pada multi command (jalankan 1 saja command meski ada 2 command)
+        e.preventDefault() 
+        e.stopPropagation() 
     })
 })
 
-// const cards = document.querySelectorAll('.card')
-
-// cards.forEach(function(card) {
-//     card.addEventListener('click',function(e){
-//         alert('system ready')
-//     })
-// })
-
-// const container = document.querySelector(".container");
-
-// container.addEventListener("click", function(e) {
-//   if (e.target.className === "close") {
-//     e.target.parentElement.style.display = "none";
-//     e.preventDefault()
-//   }
-// });
